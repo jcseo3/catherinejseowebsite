fc12ae0891acf44186a0536769c2469aa436a505
jcseo3 <jseo@colgate.edu>
2022-02-21 14:57:00 -0500

    init website

---
+++ pages/musings.js
@@ -0,0 +1,6 @@
+export default function() {
+    return (<div>
+        plant mom, adventurous fashioner, admirer of various art forms, music hummer, 90s music, fiction reader, solo traveler, interior designer with great taste, chatty if you know me, lover of stories, korean history, exceptionally taleneted gift giver, random baker, fat activist, movies
+    </div>)
+    
+}
---
+++ pages/travels.js
@@ -0,0 +1,7 @@
+export default function travels() {
+    return (
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12084.78206161046!2d-73.94773805!3d40.77971565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1641074185463!5m2!1sen!2sus" width="600" height="450" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    )
+}
